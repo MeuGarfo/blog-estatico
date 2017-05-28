b684fb76fb1b364f41e367c85f1497e306900165
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-05-28 03:11:08 -0300

    alt duplicado

--- view/inc/logo.php
+++ view/inc/logo.php
@@ -2,7 +2,7 @@
     <div class="row-fluid">
         <div class="span2 offset1 visible-desktop">
             <a href="/" title="<?php print SITE_NAME; ?>">
-                <img src="/img/logo.png" alt="" style="width:100%;margin:-7px 0;" alt="<?php print SITE_NAME; ?>">
+                <img src="/img/logo.png" style="width:100%;margin:-7px 0;" alt="<?php print SITE_NAME; ?>">
             </a>
         </div>
         <div class="span6">
