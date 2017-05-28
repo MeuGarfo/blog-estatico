817e34ab2dd47a1a58833765c1c13710473d4dac
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-05-28 03:08:24 -0300

    folha de estilo externa

--- view/inc/bs2.php
+++ view/inc/bs2.php
@@ -1,33 +1,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta property="og:image" content="https://hackergaucho.com/img/logo.png">
 <link href="/bs2/css/bootstrap.css" rel="stylesheet">
-<style>
-body {
-    padding-top: 50px;
-}
-.navbar .nav > .active > a, .navbar .nav > .active > a:hover, .navbar .nav > .active > a:focus{
-    background-color: #eeeeee;
-    box-shadow:none;
-}
-.gray{
-    color:#999;
-}
-.navbar-inner{
-    background-color:white;
-    background-image:none;
-    border: 1px solid #ddd;
-}
-.navbar-fixed-top .navbar-inner, .navbar-static-top .navbar-inner{
-    box-shadow: none;
-}
-#title{
-    margin-top:-5px;
-}
-@media (max-width: 767px) {
-    #title{
-        margin-top:-20px;
-    }
-}
-</style>
+<link href="/bs2/custom.css" rel="stylesheet">
 <link href="/bs2/css/bootstrap-responsive.css" rel="stylesheet">
 <link rel="icon" type="image/png" href="/img/logo.png" />
